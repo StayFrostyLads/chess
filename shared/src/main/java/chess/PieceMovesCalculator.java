685e560b7134927677f4eb73d4474752cbb44f70
StayFrostyLads <thejacknebeker@gmail.com>
2025-05-05 22:40:32 -0600

    Changed equals() method in ChessMove to be on separate lines so that it's more readable. Added functionality for Pawn moves (need to add promotion still)

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -2,27 +2,31 @@ package chess;
 
 import java.util.*;
 
+import static chess.ChessGame.TeamColor.WHITE;
+
 public class PieceMovesCalculator {
-    private final int[][] BISHOP_DIRECTION = {
+    private static final int[][] BISHOP_DIRECTION = {
             {1, 1,}, {1, -1}, {-1, 1}, {-1, -1}
     };
-    private final int[][] ROOK_DIRECTION = {
+    private static final int[][] ROOK_DIRECTION = {
             {1, 0}, {-1, 0}, {0, 1}, {0, -1}
     };
-    private final int[][] QUEEN_DIRECTION = {
+    private static final int[][] QUEEN_DIRECTION = {
             {1, 1,}, {1, -1}, {-1, 1}, {-1, -1},
             {1, 0}, {-1, 0}, {0, 1}, {0, -1}
     };
-    private final int[][] KNIGHTS_MOVES = {
+    private static final int[][] KNIGHTS_MOVES = {
             {2,1},{2,-1},{-2,1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}
     };
-    private final int[][] KING_MOVES = {
+    private static final int[][] KING_MOVES = {
             {1,1},{1,0},{1,-1},{0,1},{0,-1},{-1,1},{-1,0},{-1,-1}
     };
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position){
         ChessPiece piece = board.getPiece(position);
-        if (piece == null) return Collections.emptyList();
+        if (piece == null) {
+            return Collections.emptyList();
+        }
         return switch (piece.getPieceType()) {
             case KING -> calculateKingMoves(board, position);
             case QUEEN -> calculateQueenMoves(board, position);
@@ -36,13 +40,14 @@ public class PieceMovesCalculator {
     private Collection<ChessMove> createSlidingPieces(ChessBoard board, ChessPosition startPosition, int[][] directions) {
         List<ChessMove> moves = new ArrayList<>();
         for (var direction : directions) {
-            int rowDirection = direction[0];
-            int colDirection = direction[1];
+            int rowDirection = direction[0], colDirection = direction[1];
             int row = startPosition.getRow(), col = startPosition.getColumn();
             while (true) {
                 row += rowDirection;
                 col += colDirection;
-                if (row < 1 || row > 8 || col < 1 || col > 8) break;
+                if (row < 1 || row > 8 || col < 1 || col > 8) {
+                    break;
+                }
                 ChessPosition endingPosition = new ChessPosition(row, col);
                 ChessPiece endSquarePiece = board.getPiece(endingPosition);
                 if (endSquarePiece == null) {
@@ -83,7 +88,70 @@ public class PieceMovesCalculator {
         return new ArrayList<>();
     }
 
+    /**
+     * 
+     * @param board The chess board
+     * @param possibleMoves A list of the possible moves available
+     * @param startPosition The starting position of the piece moving
+     * @param rowDirection The direction of the row
+     * @param colDirection The direction of the column
+     * @param promotionPiece Whether the pawn can promote
+     */
+    private void helpPawnMoves(ChessBoard board, List<ChessMove> possibleMoves, ChessPosition startPosition, int rowDirection, int colDirection, ChessPiece.PieceType promotionPiece) {
+        int row = startPosition.getRow() + rowDirection, col = startPosition.getColumn() + colDirection;
+        if (row < 1 || row > 8 || col < 1 || col > 8) {
+            return;
+        }
+        ChessPosition endPosition = new ChessPosition(row, col);
+        ChessPiece endSquarePiece = board.getPiece(endPosition);
+        ChessPiece piece = board.getPiece(startPosition);
+        if (colDirection == 0) {
+            if (endSquarePiece == null) {
+                possibleMoves.add(new ChessMove(startPosition, endPosition, promotionPiece));
+            } else {
+                if (endSquarePiece != null && endSquarePiece.getTeamColor() != piece.getTeamColor()) {
+                    possibleMoves.add(new ChessMove(startPosition, endPosition, promotionPiece));
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param position The position of the pawn to be offset
+     * @param rowDirection The direction of the row
+     * @param colDirection The direction of the column
+     * @return The now offset position of the pawn
+     */
+    private ChessPosition offset(ChessPosition position, int rowDirection, int colDirection) {
+        return new ChessPosition(position.getRow() + rowDirection, position.getColumn() + colDirection);
+    }
+
+    /**
+     *
+     * @param board The chess board
+     * @param position The position of the pawn
+     * @return An array list of moves that the pawn can make
+     */
     public Collection<ChessMove> calculatePawnMoves(ChessBoard board, ChessPosition position) {
-        return new ArrayList<>();
+        List<ChessMove> moves = new ArrayList<>();
+        ChessPiece pawn = board.getPiece(position);
+        if (pawn == null) {
+            return moves;
+        }
+        int direction = pawn.getTeamColor() == WHITE ? +1 : -1;
+        helpPawnMoves(board, moves, position, direction, 0, null);
+
+        int homeRank = pawn.getTeamColor() == WHITE ? 2 : 7;
+        if (position.getRow() == homeRank) {
+            if (board.getPiece(offset(position, direction, 0)) == null) {
+                helpPawnMoves(board, moves, position, direction*2, 0, null);
+            }
+        }
+
+        helpPawnMoves(board, moves, position, direction, 1, null);
+        helpPawnMoves(board, moves, position, direction, -1, null);
+
+        return moves;
     }
 }
