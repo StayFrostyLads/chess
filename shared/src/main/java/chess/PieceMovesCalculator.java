421d0e6b34b729226c435cf957dd724f6c89b192
StayFrostyLads <thejacknebeker@gmail.com>
2025-05-05 23:01:36 -0600

    Added helper method createSteppingPieces() for calculateKingMoves() and calculateKnightMoves(), going to try and think of a better naming convention rather than having Stepping and Sliding.

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -5,13 +5,13 @@ import java.util.*;
 import static chess.ChessGame.TeamColor.WHITE;
 
 public class PieceMovesCalculator {
-    private static final int[][] BISHOP_DIRECTION = {
+    private static final int[][] BISHOP_MOVES = {
             {1, 1,}, {1, -1}, {-1, 1}, {-1, -1}
     };
-    private static final int[][] ROOK_DIRECTION = {
+    private static final int[][] ROOK_MOVES = {
             {1, 0}, {-1, 0}, {0, 1}, {0, -1}
     };
-    private static final int[][] QUEEN_DIRECTION = {
+    private static final int[][] QUEEN_MOVES = {
             {1, 1,}, {1, -1}, {-1, 1}, {-1, -1},
             {1, 0}, {-1, 0}, {0, 1}, {0, -1}
     };
@@ -22,7 +22,7 @@ public class PieceMovesCalculator {
             {1,1},{1,0},{1,-1},{0,1},{0,-1},{-1,1},{-1,0},{-1,-1}
     };
 
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position){
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         ChessPiece piece = board.getPiece(position);
         if (piece == null) {
             return Collections.emptyList();
@@ -37,6 +37,14 @@ public class PieceMovesCalculator {
         };
     }
 
+    /**
+     * The helper method for the moving functionality of the Queen, Bishop, and Rook
+     *
+     * @param board The chess board
+     * @param startPosition The starting position of the piece being moved
+     * @param directions An array of arrays that passes in the moves possible for a queen, bishop, or rook
+     * @return moves: An array list of possible moves for the king or knight based on the current position
+     */
     private Collection<ChessMove> createSlidingPieces(ChessBoard board, ChessPosition startPosition, int[][] directions) {
         List<ChessMove> moves = new ArrayList<>();
         for (var direction : directions) {
@@ -48,13 +56,13 @@ public class PieceMovesCalculator {
                 if (row < 1 || row > 8 || col < 1 || col > 8) {
                     break;
                 }
-                ChessPosition endingPosition = new ChessPosition(row, col);
-                ChessPiece endSquarePiece = board.getPiece(endingPosition);
+                ChessPosition endPosition = new ChessPosition(row, col);
+                ChessPiece endSquarePiece = board.getPiece(endPosition);
                 if (endSquarePiece == null) {
-                    moves.add(new ChessMove(startPosition, endingPosition, null));
+                    moves.add(new ChessMove(startPosition, endPosition, null));
                 } else {
                     if (endSquarePiece.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                        moves.add(new ChessMove(startPosition, endingPosition, null));
+                        moves.add(new ChessMove(startPosition, endPosition, null));
                     }
                     break;
                 }
@@ -63,35 +71,57 @@ public class PieceMovesCalculator {
         return moves;
     }
 
-    private Collection<ChessMove> createStepMoves(ChessBoard board, ChessPosition startPosition, int[][] directions) {
+    /**
+     * A helper method for the moving functionality of the King and Knight
+     *
+     * @param board The chess board
+     * @param startPosition The starting position of the piece being moved
+     * @param directions An array of arrays that passes in the moves possible for a king or knight
+     * @return moves: An array list of possible moves for the king or knight based on the current position
+     */
+    private Collection<ChessMove> createSteppingPieces(ChessBoard board, ChessPosition startPosition, int[][] directions) {
         List<ChessMove> moves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(startPosition);
+
+        for (var direction : directions) {
+            int row = direction[0] + startPosition.getRow();
+            int col = direction[1] + startPosition.getColumn();
+            if (row < 1 || row > 8 || col < 1 || col > 8) {
+                continue;
+            }
+            ChessPosition endPosition = new ChessPosition(row, col);
+            ChessPiece endSquare = board.getPiece(endPosition);
+            if (endSquare == null || endSquare.getTeamColor() != piece.getTeamColor()) {
+                moves.add(new ChessMove(startPosition, endPosition, null));
+            }
+        }
         return moves;
     }
 
     public Collection<ChessMove> calculateKingMoves(ChessBoard board, ChessPosition position) {
-        return new ArrayList<>();
+        return createSteppingPieces(board, position, KING_MOVES);
     }
 
     public Collection<ChessMove> calculateQueenMoves(ChessBoard board, ChessPosition position) {
-        return createSlidingPieces(board, position, QUEEN_DIRECTION);
+        return createSlidingPieces(board, position, QUEEN_MOVES);
     }
 
     public Collection<ChessMove> calculateRookMoves(ChessBoard board, ChessPosition position) {
-        return createSlidingPieces(board, position, ROOK_DIRECTION);
+        return createSlidingPieces(board, position, ROOK_MOVES);
     }
 
     public Collection<ChessMove> calculateBishopMoves(ChessBoard board, ChessPosition position) {
-        return createSlidingPieces(board, position, BISHOP_DIRECTION);
+        return createSlidingPieces(board, position, BISHOP_MOVES);
     }
 
     public Collection<ChessMove> calculateKnightMoves(ChessBoard board, ChessPosition position) {
-        return new ArrayList<>();
+        return createSteppingPieces(board, position, KNIGHTS_MOVES);
     }
 
     /**
      * 
      * @param board The chess board
-     * @param possibleMoves A list of the possible moves available
+     * @param possibleMoves A list of the possible moves available for the pawn based on the position
      * @param startPosition The starting position of the piece moving
      * @param rowDirection The direction of the row
      * @param colDirection The direction of the column
