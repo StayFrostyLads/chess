8157851f9f1370e645958402d8e2820449c1a3a2
StayFrostyLads <thejacknebeker@gmail.com>
2025-05-19 19:32:40 -0600

    Base test web server has been set up, and the necessary maven dependencies were added to the project

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,30 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("/web");
+
+        // Register your endpoints and handle exceptions here.
+        Spark.get("/", (req, res) -> {
+                    // either redirect:
+                    res.redirect("/index.html");
+                    return null;
+                });
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
